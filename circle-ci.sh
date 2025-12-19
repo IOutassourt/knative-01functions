e5a9f70fa5523fede37806d3226d711c5c192500
IOutassourt <i.outassourt.math@gmail.com>
2025-12-19 07:14:06 +0100

    updating circleci config

--- circle-ci.sh
+++ circle-ci.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Run the build_and_test workflow
-circleci local execute --job build
+# circleci local execute <job-name>
+# config: ./.circleci/config.yml
+circleci local execute test
